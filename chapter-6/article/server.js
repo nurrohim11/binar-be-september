6974e95510c369559f454ea24cb97f370a1c5420
Rohim Alkhattabi <nur.rohim@arvis.id>
2022-11-05 09:48:55 +0700

    update chpater 6

---
+++ chapter-6/article/server.js
@@ -0,0 +1,77 @@
+const express = require('express')
+const app = express()
+const port = 2006
+const { article } = require('./models')
+
+// import library and file json swagger
+const swaggerUI = require('swagger-ui-express')
+const swaggerJson = require('./docs/swagger.json')
+
+app.use(express.json())
+
+app.get('/health-check', (request, response) =>{
+  response.json({
+    message:'Health check success'
+  })
+})
+
+// setup url swagger
+app.use('/api/v0/docs', swaggerUI.serve, swaggerUI.setup(swaggerJson))
+
+app.get('/api/v0/article', async (req, res)=>{
+  const data = await article.findAll({
+    where:{
+      approved:true
+    }
+  })
+  if (data.length == 0) {
+    return res.status(404).json({
+      status:404,
+      message:'Data tidak ditemukan'
+    })
+  }
+  res.status(200).json({
+    status:200,
+    message:'Successfull',
+    response:data
+  })
+})
+
+app.post('/api/v0/article', async(req, res)=>{
+  const { title="", body="", approved=false } = req.body
+  if (title == "" || body == "") {
+    if (title == "") {
+      return res.status(400).json({
+        status:400,
+        message:'Title tidak kosong'
+      })
+    }
+    if (body == "") {
+      return res.status(400).json({
+        status:400,
+        message:'Body tidak kosong'
+      })
+    }
+  } else {
+    const create = await article.create({
+      title:title,
+      body:body,
+      approved:approved
+    })
+    if (create == null) {
+      return res.status(500).json({
+        status:500,
+        message:'Server error'
+      })
+    }
+    res.status(200).json({
+      status:200,
+      message:'Successfull insert a new data',
+      response:create
+    })
+  }
+})
+
+app.listen(port, () =>{
+  console.log(`server running at port ${port}`)
+})
--- pegawai.md
+++ pegawai.md
@@ -10,3 +10,10 @@ npx sequelize-cli model:generate --name pegawai --attributes nama:string,jenis_k
 npx sequelize-cli model:generate --name pegawai --attributes nama:string,jenis_kelamin:string,tgl_masuk:date,email:string
 <!-- running miggrate -->
 sequelize db:migrate
+
+
+- article
+  - title, body, approved
+
+  npx sequelize-cli model:generate --name article --attributes title:string,body:string,approved:boolean  
+  sequelize db:migrate
